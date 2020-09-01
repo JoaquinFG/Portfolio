395d2ae10d0a0a54aff1cfc97e4230452a34264e
Joaquin_FG <48914838+JoaquinFG@users.noreply.github.com>
2020-09-01 22:12:00 +0100

    Main and routes

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import './App.css';
-import Header from './components/header';
+import React from "react";
+import "./App.css";
+import { Layout, Header, Navigation, Drawer , Content } from "react-mdl";
+import Main from "./components/main";
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Layout className="bg-img">
+        <Header transparent title="My Portfolio">
+          <Navigation>
+            <Link to="/resume">Resume</Link>
+            <Link to="/aboutme">About me</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
+          </Navigation>
+        </Header>
+        <Drawer title="My Portfolio">
+          <Navigation>
+            <Link to="/resume">Resume</Link>
+            <Link to="/aboutme">About me</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
+          </Navigation>
+        </Drawer>
+        <Content>
+          <Main />
+        </Content>
+      </Layout>
     </div>
   );
 }
---
+++ myportfolio/src/components/landingpage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class landingpage extends Component {
+    render() {
+        return (
+          <div>
+            <h1>Landing Page</h1>
+          </div>
+        );
+    }
+}
