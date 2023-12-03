98fbc484b8b45d321a5e68b232476b3e8aec2f44
joaquin.ferreira <133866030+JoaquinFerreiraBabel@users.noreply.github.com>
2023-12-03 21:43:23 +0100

    sections completed (not pt and test)

---
+++ portfolio.bootstrap.2023/index.js
@@ -0,0 +1,5 @@
+const webLabel = "www.canditec.com";
+const phoneLabel = "(+34) 686 592 376";
+const phoneParagraph = "(+34) 686 592 376"
+const emailLabel = "joaquin.ferreira@canditec.com";
+const emailParagraph = "joaquin.ferreira@canditec.com"
