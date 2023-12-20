a2d2a14294c093a8637e178a19cbbf31275c79ee
joaquin.ferreira <133866030+JoaquinFerreiraBabel@users.noreply.github.com>
2023-12-20 08:18:06 +0100

    fix testimonials no pro

--- portfolio.bootstrap.2023/index.js
+++ portfolio.bootstrap.2023/index.js
@@ -1,5 +1,5 @@
-const webLabel = "www.canditec.com";
+const webLabel = "www.joaquinferreira.com";
 const phoneLabel = "(+34) 686 592 376";
 const phoneParagraph = "(+34) 686 592 376"
-const emailLabel = "joaquin.ferreira@canditec.com";
-const emailParagraph = "joaquin.ferreira@canditec.com"
+const emailLabel = "joaquin.ferreira@gmail.com";
+const emailParagraph = "joaquin.ferreira@gmail.com"
