7b78ea5b9447d3c268e0e010900f01f7ddcdcba5
joaquin.ferreira <133866030+JoaquinFerreiraBabel@users.noreply.github.com>
2024-01-15 08:34:26 +0100

    fix email

--- portfolio.bootstrap.2023/index.js
+++ portfolio.bootstrap.2023/index.js
@@ -1,5 +1,5 @@
 const webLabel = "www.joaquinferreira.com";
 const phoneLabel = "(+34) 686 592 376";
 const phoneParagraph = "(+34) 686 592 376"
-const emailLabel = "joaquin.ferreira@gmail.com";
-const emailParagraph = "joaquin.ferreira@gmail.com"
+const emailLabel = "joaquinferreirag@gmail.com";
+const emailParagraph = "joaquinferreirag@gmail.com"
